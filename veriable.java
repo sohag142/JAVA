70a1c89c439fe474f99f630ac60e5d27f29f4f22
Sohag <134754736+sohag142@users.noreply.github.com>
2023-07-28 10:02:29 +0600

    Add files via upload

---
+++ veriable.java
@@ -0,0 +1,26 @@
+package sohag;
+
+public class veriable {
+
+	public static void main(String[] args) {
+		short fristnumber;
+		float secondnumber;
+		float result;
+		
+		fristnumber = 20;
+		secondnumber = 30;
+		
+		result =  secondnumber / fristnumber;
+		
+		System.out.print("The Frist NUmber : ");
+		System.out.println(fristnumber);
+		System.out.print("The Second NUmber : ");
+		System.out.println(secondnumber);
+		System.out.print("Final Result : ");
+		System.out.println(result);
+		
+		
+
+	}
+
+}
